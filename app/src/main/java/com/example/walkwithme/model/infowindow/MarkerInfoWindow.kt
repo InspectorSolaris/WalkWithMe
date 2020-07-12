e3c4d61d925480567d94dabc4c4ec9d714068832
Dima <dima.avanesov@yandex.ru>
2020-07-12 19:26:43 +0700

    Closing InfoWindow on tap

--- app/src/main/java/com/example/walkwithme/model/infowindow/MarkerInfoWindow.kt
+++ app/src/main/java/com/example/walkwithme/model/infowindow/MarkerInfoWindow.kt
@@ -1,5 +1,6 @@
 package com.example.walkwithme.model.infowindow
 
+import android.view.View
 import androidx.cardview.widget.CardView
 import com.example.walkwithme.R
 import kotlinx.android.synthetic.main.marker_info_window.view.*
@@ -16,6 +17,7 @@ class MarkerInfoWindow(layoutResId: Int, mapView: MapView?) : InfoWindow(layoutR
         layout.Number.text = marker.title
         layout.Category.text = marker.snippet
         layout.Description.text = marker.subDescription
+        layout.setOnClickListener { closeAllInfoWindowsOn(mapView) }
     }
 
     override fun onClose() {}
